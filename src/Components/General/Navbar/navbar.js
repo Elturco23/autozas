db75fdb00bf6435e39669a7453e2953555917dfa
Elturco23 <jriano@uninorte.edu.co>
2022-03-08 14:23:41 -0500

    cambios navbar

--- src/Components/General/Navbar/navbar.js
+++ src/Components/General/Navbar/navbar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {NavDropdown,Navbar,Container, Nav,Form,FormControl,Button} from 'react-bootstrap' ;
+import './navbar.css';
 function navbar() {
     return (
-        <div>
-            <Navbar bg="light" expand="lg">
+        <div className='container-principal-navbar'>
+            <Navbar className='container-navbar' expand="lg">
   <Container fluid>
     <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
     <Navbar.Toggle aria-controls="navbarScroll" />
@@ -27,15 +28,7 @@ function navbar() {
           Link
         </Nav.Link>
       </Nav>
-      <Form className="d-flex">
-        <FormControl
-          type="search"
-          placeholder="Search"
-          className="me-2"
-          aria-label="Search"
-        />
-        <Button variant="outline-success">Search</Button>
-      </Form>
+      
     </Navbar.Collapse>
   </Container>
 </Navbar>
