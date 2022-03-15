bf4a90578dc764945b8293b4019c43fd65f34a62
Elturco23 <49315015+Elturco23@users.noreply.github.com>
2022-03-14 22:24:10 -0500

    ....

--- src/Components/General/Navbar/navbar.js
+++ src/Components/General/Navbar/navbar.js
@@ -1,39 +1,40 @@
 import React from 'react';
-import {NavDropdown,Navbar,Container, Nav,Form,FormControl,Button} from 'react-bootstrap' ;
+import { Navbar, Container, Nav } from 'react-bootstrap';
 import './navbar.css';
 import logo from '../../../img/contorno.png';
-import fondo from '../../../img/llanta22.png';
+
 function navbar() {
     return (
-        
-            <Navbar className='sticky-top' expand="lg" style={{backgroundColor:"#20A39E"}}>
-  <Container fluid>
-    <Navbar.Brand href="#"><img src={logo} height='80px'/></Navbar.Brand>
-    
-    <Navbar.Toggle aria-controls="navbarScroll" />
-    <Navbar.Collapse id="navbarScroll">
-      <Nav
-        className="Scroll"
-        style={{ maxHeight: '100px'}}
-        navbarScroll
-      >
-        <Nav.Link href="#action1"><i class='fa fa-home'></i>Inicio</Nav.Link>
-        <Nav.Link href="#action2"><i class='fas fa-toolbox'></i>Talleres</Nav.Link>
-        
-        
-        <Nav.Link href="#" >
+        <Navbar className='sticky-top' expand="lg" style={{backgroundColor:"#20A39E"}}>
+        <Container fluid>
+          <Navbar.Brand href="#"><img src={logo} height='80px'/></Navbar.Brand>
           
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="Scroll"
+              
+              navbarScroll
+            >
+              <Nav.Link href="#action1"><i class='fa fa-home'></i>Inicio</Nav.Link>
+              <Nav.Link href="#action2"><i class='fas fa-toolbox'></i>Talleres</Nav.Link>
+              
+              
+              <Nav.Link href="#" >
+                
+              
+      <i class='fa fa-phone-square'></i>Contáctenos
+              </Nav.Link>
+              <Nav.Link href="#action2">
+              <i class='fa fa-question-circle-o'></i>¿Quiénes somos?</Nav.Link>
+            </Nav>
+            
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
         
-<i class='fa fa-phone-square'></i>Contáctenos
-        </Nav.Link>
-        <Nav.Link href="#action2">
-        <i class='fa fa-question-circle-o'></i>¿Quiénes somos?</Nav.Link>
-      </Nav>
-      
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
-        
+
     );
 }
 
